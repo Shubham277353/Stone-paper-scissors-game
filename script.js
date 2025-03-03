512185c7b6bf88c92d50d78e853a88f08b42afeb
Shubham277353 <golubhandari405@gmail.com>
2025-03-03 22:52:37 +0530

    bug was not fixed

--- script.js
+++ script.js
@@ -1,45 +1,40 @@
-
 let stone = document.querySelector(".stone");
 let paper = document.querySelector(".paper");
 let scissors = document.querySelector(".scissors");
 let userScore = document.querySelector("#userscore");
 let compScore = document.querySelector("#compscore");
 const message = document.querySelector("#msg");
-const finalMessage = document.querySelector("#finalScore")
-const popUp = document.querySelector("#popup")
-const restart = document.querySelector("#restart-button")
-
-
+const finalMessage = document.querySelector("#finalScore");
+const popUp = document.querySelector("#popup");
+const restart = document.querySelector("#restart-button");
 
 function getComputerChoice() {
-    const choices = ["Stone", "Paper", "Scissors"];
-    const randomIndex = Math.floor(Math.random() * 3);
-    return choices[randomIndex];
+  const choices = ["Stone", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  return choices[randomIndex];
 }
 
 restart.addEventListener("click", () => {
-  round  = 0;
+  round = 0;
   humanScore = 0;
-  computerScore = 0; 
+  computerScore = 0;
   userScore.textContent = 0;
   compScore.textContent = 0;
 
   console.log(humanScore);
   console.log(computerScore);
   popUp.style.display = "none";
-})
-
+});
 
 let humanScore = 0;
 let computerScore = 0;
 let round = 0;
 
-
 function playRound(newHumanChoice) {
   round++;
   let computerchoice = getComputerChoice();
-  console.log(`Your choice = ${newHumanChoice}`)
-  console.log(`Opponent's choice = ${computerchoice}`)
+  console.log(`Your choice = ${newHumanChoice}`);
+  console.log(`Opponent's choice = ${computerchoice}`);
   if (newHumanChoice === computerchoice) {
     message.textContent = "Oh! It's a draw.";
   } else if (
@@ -54,35 +49,28 @@ function playRound(newHumanChoice) {
     computerScore += 1;
     message.textContent = "Oops!! , You lost . ";
     message.style.backgroundColor = "red";
-
-    
   }
 
   userScore.textContent = humanScore;
   compScore.textContent = computerScore;
 
-  if(round === 5){
+  if (round === 5) {
     endGame();
   }
 }
 
+function endGame() {
+  if (humanScore === computerScore) {
+    finalMessage.textContent = "Wow, it's a draw. Well played!";
+  } else if (humanScore > computerScore) {
+    finalMessage.textContent = "Congratulations!!!! You won!";
+  } else {
+    finalMessage.textContent = "Sadly, you lost. Well played though.";
+  }
 
-  function endGame() {
-    if (humanScore === computerScore) {
-        finalMessage.textContent = "Wow, it's a draw. Well played!";
-    } else if (humanScore > computerScore) {
-      finalMessage.textContent = "Congratulations!!!! You won!";
-    } else {
-      finalMessage.textContent = "Sadly, you lost. Well played though.";
-    }
-
-  popUp.style.display = "flex"
-
+  popUp.style.display = "flex";
 }
 
-
-stone.addEventListener("click",() => playRound("Stone"));
-paper.addEventListener("click",() => playRound("Paper"));
-scissors.addEventListener("click",() => playRound("Scissors"));
-
-
+stone.addEventListener("click", () => playRound("Stone"));
+paper.addEventListener("click", () => playRound("Paper"));
+scissors.addEventListener("click", () => playRound("Scissors"));
